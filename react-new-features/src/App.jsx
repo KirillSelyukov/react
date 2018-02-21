d9ea9636bac4d277809141452b468eb8127c1dd8
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-02-21 10:55:50 +0200

    Toggle Useroutput

--- react-new-features/src/App.jsx
+++ react-new-features/src/App.jsx
@@ -6,20 +6,36 @@ import { UserOutput } from '../src/UserOutput/UserOutput'
 
 class App extends Component {
   state = {
-    username: 'ololo'
+    username: 'Musius Scaevola',
+    showUserOutput: false
   }
+
   onChangeHandler = (event) => {
     this.setState({
       username: event.target.value
     });
   }
 
+  toggleOutputhandler = () => {
+    console.log(this.state.showUserOutput);
+    this.setState({
+      showUserOutput: !this.state.showUserOutput
+    })
+  }
+
   render() {
     return (
       <div>
-        <UserInput changed={this.onChangeHandler} username = {this.state.username} ></UserInput>
-        <UserOutput userName={this.state.username}></UserOutput>
-        <UserOutput userName='Scaevola'></UserOutput>
+        <button onClick={this.toggleOutputhandler}>Show Output</button>
+        <UserInput changed={this.onChangeHandler} username={this.state.username} ></UserInput>
+        {this.state.showUserOutput
+          ?
+          <div>
+            <UserOutput userName={this.state.username}></UserOutput>
+            <UserOutput userName='Scaevola'></UserOutput>
+          </div>
+          : null
+        }
       </div>
     );
   }
