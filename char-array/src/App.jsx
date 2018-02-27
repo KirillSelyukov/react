7d06f31d63cb6672d01efc45f9f5b89bea0ade4b
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-02-27 10:57:01 +0200

    Add event listener onChange
    Output length of input value to separate paragraph

--- char-array/src/App.jsx
+++ char-array/src/App.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    inputLength: 0
+  }
+  onChangeHandler = (e) => {
+    this.setState({ inputLength: e.target.value.length });
+  }
   render() {
     return (
       <div className="App">
-      <input />
+        <input onChange={this.onChangeHandler} />
+        <p>Length of input: {this.state.inputLength}</p>
 
       </div>
     );
