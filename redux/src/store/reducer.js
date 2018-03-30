b399cac9e26279e81a5e54f3f437fa08a0ba480a
Kirill Selyukov <kselukov@gmail.com>
2018-03-30 12:22:33 +0300

    Pass data into reducer

--- redux/src/store/reducer.js
+++ redux/src/store/reducer.js
@@ -6,6 +6,24 @@ const reducer = (state = initialState, action) => {
             counter: state.counter + 1
         }
     }
+    if (action.type === 'DECREMENT') {
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        return {
+            ...state,
+            counter: state.counter + action.val
+        }
+    }
+    if (action.type === 'SUBSTRACT') {
+        return {
+            ...state,
+            counter: state.counter - action.val
+        }
+    }
 
     return state;
 }
