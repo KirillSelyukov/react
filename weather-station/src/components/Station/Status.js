7baa3f7f187aaf33df0a53f27535c698f1dc9602
Kirill Selyukov <kselukov@gmail.com>
2018-04-27 20:02:21 +0300

    rm obsolet

--- weather-station/src/components/Station/Status.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-const Status = (props) => {
-    return props.enabled ? <p style={{ color: 'green' }}>ONLINE</p> : <p style={{ color: 'red' }}>OFFLINE</p>;
-}
-
-export default Status;
--- weather-station/src/components/Stations/Stations.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Station from '../Station/Station';
-
-export default function Stations({ names }) {
-    return names.map(name => <Station key={name} name={name} />)
-}
